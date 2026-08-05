2fbc4be4e5008af1ca7611d4ef7946754d190446
John LeCorn <63716175+johngraven75@users.noreply.github.com>
2026-08-05 00:39:47 -0400

    feat(ios): activate Build 1.09 user interface

--- CinaVaultIOS/CinaVaultIOSApp.swift
+++ CinaVaultIOS/CinaVaultIOSApp.swift
@@ -11,13 +11,8 @@ private enum RecoveryMonitor {
         return "CV-IOS-\(String(Int(Date().timeIntervalSince1970), radix: 16).uppercased())"
     }
 
-    static func markActive() {
-        UserDefaults.standard.set(true, forKey: activeKey)
-    }
-
-    static func markCleanBackground() {
-        UserDefaults.standard.set(false, forKey: activeKey)
-    }
+    static func markActive() { UserDefaults.standard.set(true, forKey: activeKey) }
+    static func markCleanBackground() { UserDefaults.standard.set(false, forKey: activeKey) }
 }
 
 @main
@@ -29,19 +24,15 @@ struct CinaVaultIOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            CinaVaultRootView(model: model, recoveryDiagnostic: $recoveryDiagnostic)
+            Build109RootView(model: model, recoveryDiagnostic: $recoveryDiagnostic)
                 .preferredColorScheme(.dark)
                 .background(CVColor.ink)
                 .onChange(of: scenePhase) { _, phase in
                     switch phase {
-                    case .active:
-                        RecoveryMonitor.markActive()
-                    case .background:
-                        RecoveryMonitor.markCleanBackground()
-                    case .inactive:
-                        break
-                    @unknown default:
-                        break
+                    case .active: RecoveryMonitor.markActive()
+                    case .background: RecoveryMonitor.markCleanBackground()
+                    case .inactive: break
+                    @unknown default: break
                     }
                 }
         }
